a692014552ca67a8f4dcf8a6f436cc5181bb2ba4
onlyxxp <onlyxxp@gmail.com>
2025-01-09 22:44:07 +0800

    Update entrypoint.sh

--- ss/entrypoint.sh
+++ ss/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 echo "start_apps begin"
-echo $SERVER_ADDR
-echo $PASSWORD
-
-
 
 echo "[snell-server]
 listen = $LISTEN
@@ -15,5 +11,15 @@ cat /etc/snell/snell-server.conf
 snell-server -c /etc/snell/snell-server.conf&
 
 
+
+echo '{
+    "server":"::",
+    "server_port": '"$SS_LISTEN"',
+    "mode":"'"$SS_MOD"'",
+    "password": "'"$PASSWORD"'",
+    "timeout": "'"$SS_TIMEOUT"'",
+    "method": "'"$SS_METHOD"'"
+}' > /etc/shadowsocks-rust/config.json
 cat /etc/shadowsocks-rust/config.json
+
 exec "$@"
