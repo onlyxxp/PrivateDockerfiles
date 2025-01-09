3cc59f73497a9d6b1998a61e444675cdff7803fe
onlyxxp <onlyxxp@gmail.com>
2025-01-09 21:35:50 +0800

    Update entrypoint.sh

--- ss/entrypoint.sh
+++ ss/entrypoint.sh
@@ -4,4 +4,16 @@ echo "start_apps begin"
 echo $SERVER_ADDR
 echo $PASSWORD
 
+
+
+echo "[snell-server]
+listen = $LISTEN
+psk = $PSK
+ipv6 = false" > /etc/snell/snell-server.conf
+
+cat /etc/snell/snell-server.conf
+snell-server -c /etc/snell/snell-server.conf
+
+
+
 exec "$@"
