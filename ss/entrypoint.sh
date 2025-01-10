d85a7f0c45b21bbd87ac38bfbf0c0d6b782ed739
onlyxxp <onlyxxp@gmail.com>
2025-01-10 21:09:48 +0800

    Update entrypoint.sh

--- ss/entrypoint.sh
+++ ss/entrypoint.sh
@@ -3,6 +3,17 @@
 
 echo ":::entrypoint begin:::"
 
+# 添加默认环境变量
+export SS_LISTEN=8388
+export SS_PWD=12aa.345g 
+export SS_METHOD=aes-256-gcm
+export SS_MOD=tcp_and_udp 
+export SS_TIMEOUT=600
+
+# 添加默认环境变量
+export PSK=uYQwNqZbaIOMiZ6Zni8v5x0M09Y8bSK
+export LISTEN=0.0.0.0:9000
+
 echo "[snell-server]
 listen = $LISTEN
 psk = $PSK
@@ -12,7 +23,7 @@ cat /etc/snell/snell-server.conf
 snell-server -c /etc/snell/snell-server.conf&
 
 export SHADOW_PORT=8443
-shadow-tls --fastopen --v3 server --listen ::0:$SHADOW_PORT --server 127.0.0.1:$LISTEN --tls  publicassets.cdn-apple.com --password JsJeWtjiUyJ5ye &
+#shadow-tls --fastopen --v3 server --listen ::0:$SHADOW_PORT --server 127.0.0.1:18080 --tls  publicassets.cdn-apple.com --password JsJeWtjiUyJ5ye &
 
 echo '{
     "server": "'"$SERVER"'",
