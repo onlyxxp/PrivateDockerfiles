14889c63812df34e55349a9752d1207a587281d6
onlyxxp <onlyxxp@gmail.com>
2025-01-10 21:19:28 +0800

    Update entrypoint.sh

--- ss/entrypoint.sh
+++ ss/entrypoint.sh
@@ -3,28 +3,35 @@
 
 echo ":::entrypoint begin:::"
 
-# 添加默认环境变量
-export SS_LISTEN=8388
-export SS_PWD=12aa.345g 
-export SS_METHOD=aes-256-gcm
-export SS_MOD=tcp_and_udp 
-export SS_TIMEOUT=600
+
 
 # 添加默认环境变量
 export PSK=uYQwNqZbaIOMiZ6Zni8v5x0M09Y8bSK
-export LISTEN=0.0.0.0:9000
-
+export LISTEN=9000
+# 初始化snell config
 echo "[snell-server]
-listen = $LISTEN
+listen = 0.0.0.0:$SNELL_LISTEN
 psk = $PSK
 ipv6 = false" > /etc/snell/snell-server.conf
 
 cat /etc/snell/snell-server.conf
 snell-server -c /etc/snell/snell-server.conf&
 
-export SHADOW_PORT=8443
-#shadow-tls --fastopen --v3 server --listen ::0:$SHADOW_PORT --server 127.0.0.1:18080 --tls  publicassets.cdn-apple.com --password JsJeWtjiUyJ5ye &
+#shadow_tls
+export SHADOW_PORT=1010
+export SHADOW_PWD=JsJeWtjiUyJ5ye
+export SHADOW_HOST=publicassets.cdn-apple.com
+#shadow-tls --fastopen --v3 server --listen ::0:$SHADOW_PORT --server 127.0.0.1:$SNELL_LISTEN --tls  $SHADOW_HOST --password $SHADOW_PWD &
 
+
+# 添加默认环境变量
+export SERVER=0.0.0.0
+export SERVER_PORT=8388
+export PWD=12aa.345g 
+export METHOD=aes-256-gcm
+export MOD=tcp_and_udp 
+export TIMEOUT=600
+# 初始化ss config
 echo '{
     "server": "'"$SERVER"'",
     "server_port": '"$SERVER_PORT"',
