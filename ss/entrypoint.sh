68707a8ce1e259158d4f312defcd33335c068c90
onlyxxp <onlyxxp@gmail.com>
2025-01-10 21:27:57 +0800

    Update entrypoint.sh

--- ss/entrypoint.sh
+++ ss/entrypoint.sh
@@ -3,11 +3,6 @@
 
 echo ":::entrypoint begin:::"
 
-
-
-# 添加默认环境变量
-export PSK=uYQwNqZbaIOMiZ6Zni8v5x0M09Y8bSK
-export SNELL_LISTEN=9000
 # 初始化snell config
 echo "[snell-server]
 listen = 0.0.0.0:$SNELL_LISTEN
@@ -17,20 +12,8 @@ ipv6 = false" > /etc/snell/snell-server.conf
 cat /etc/snell/snell-server.conf
 snell-server -c /etc/snell/snell-server.conf&
 
-#shadow_tls
-export SHADOW_PORT=1010
-export SHADOW_PWD=JsJeWtjiUyJ5ye
-export SHADOW_HOST=publicassets.cdn-apple.com
 #shadow-tls --fastopen --v3 server --listen ::0:$SHADOW_PORT --server 127.0.0.1:$SNELL_LISTEN --tls  $SHADOW_HOST --password $SHADOW_PWD &
 
-
-# 添加默认环境变量
-export SERVER=0.0.0.0
-export SERVER_PORT=8388
-export PWD=12aa.345g 
-export METHOD=aes-256-gcm
-export MOD=tcp_and_udp 
-export TIMEOUT=600
 # 初始化ss config
 echo '{
     "server": "'"$SERVER"'",
