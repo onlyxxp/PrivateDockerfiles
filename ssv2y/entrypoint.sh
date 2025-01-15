3d8069d61f275d81df2df4343bd37d6ff7843ff9
onlyxxp <onlyxxp@gmail.com>
2025-01-16 03:10:22 +0800

    Update entrypoint.sh

--- ssv2y/entrypoint.sh
+++ ssv2y/entrypoint.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 # vim:sw=4:ts=4:et
 
-RED='\033[31m'
-NC='\033[0m' # No Color
-
-echo "${RED} :::entrypoint begin::: ${NC}"
+echo -e  "\033[32m :::entrypoint begin::: \033[0m"
 
 # shadow-tls
 if [ "${ENABLE_SHADOW_TLS}"  = "true" ]; then
-  echo "The environment ENABLE_SHADOW_TLS is set to: ${ENABLE_SHADOW_TLS}"
+  echo -e  "\033[33m The environment ENABLE_SHADOW_TLS is set to: ${ENABLE_SHADOW_TLS}\033[0m"
   shadow-tls --fastopen --v3 server --listen ::0:$SHADOW_PORT --server 127.0.0.1:$SNELL_LISTEN --tls  $SHADOW_HOST --password $SHADOW_PWD &
 else
-  echo "The environment ENABLE_SHADOW_TLS is  ${ENABLE_SHADOW_TLS}， not set true. not start shadow tls"
+  echo -e  "\033[33m The environment ENABLE_SHADOW_TLS is  ${ENABLE_SHADOW_TLS}， not set true. not start shadow tls\033[0m"
 fi
 
 # v2ray
@@ -26,7 +23,9 @@ psk = $PSK
 ipv6 = false" > /etc/snell/snell-server.conf
 cat /etc/snell/snell-server.conf
 # snell
-snell-server --loglevel=info -c /etc/snell/snell-server.conf
+snell-server --loglevel=info -c /etc/snell/snell-server.conf&
+
+/bin/sh
 
 #exec "$@"
 
