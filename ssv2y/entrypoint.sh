ee6091c33beebe78cfe8094370acd0376fae4762
onlyxxp <onlyxxp@gmail.com>
2025-01-16 00:59:15 +0800

    Update entrypoint.sh

--- ssv2y/entrypoint.sh
+++ ssv2y/entrypoint.sh
@@ -6,6 +6,14 @@ NC='\033[0m' # No Color
 
 echo "${RED} :::entrypoint begin::: ${NC}"
 
+# shadow-tls
+if [ "${ENABLE_SHADOW_TLS}"  = "true" ]; then
+  echo "The environment ENABLE_SHADOW_TLS is set to: ${ENABLE_SHADOW_TLS}"
+  shadow-tls --fastopen --v3 server --listen ::0:$SHADOW_PORT --server 127.0.0.1:$SNELL_LISTEN --tls  $SHADOW_HOST --password $SHADOW_PWD &
+else
+  echo "The environment ENABLE_SHADOW_TLS is  ${ENABLE_SHADOW_TLS}， not set true. not start shadow tls"
+fi
+
 # v2ray
 v2ray run -c /etc/v2ray/config.json&
 
